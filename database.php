1c8bf392e012b5b326fb3dec1f6b707b795652bf
Rajas-B <bondalerajas@gmail.com>
2021-11-05 18:28:36 +0530

    Added gitignore

--- database.php
+++ database.php
@@ -1,13 +1,16 @@
 <?php
 
+$databaseHost = 'localhost';
+$databaseName = 'phpproject';
+$databaseUsername = 'root';
+$databasePassword = 'password';
 
-$conn = mysqli_connect('127.0.0.1','astha','astha123','phpproject');
+$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 
 if(!$conn){
   echo 'Connection error';
 
 }
-
 
 
 
