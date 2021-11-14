b0bdfd9939df8588d8f789ddfd99a6c77ee08c31
Rajas-B <bondalerajas@gmail.com>
2021-11-14 12:11:49 +0530

    Added main page

---
+++ homepage.php
@@ -0,0 +1,22 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Pizzaz</title>
+        <link rel="stylesheet" href="./homepage.css" />
+    </head>
+    <body>
+        <div class="bg">
+            <div class="welcome">
+                Welcome to Pizzaz!
+                <br><br>
+                <div class="windows">
+                    <div class="window"><a href="./user/userpage.php">Order pizzas</a></div>
+                    <div class="window"><a href="./user/userpage.php">Admin login</a></div>
+                    <div class="window"><a href="./user/userpage.php">Delivery person login</a></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- user/edit_profile.php
+++ user/edit_profile.php
@@ -1,5 +1,5 @@
 <?php 
-include_once 'database.php';
+include_once '../database.php';
 session_start();
 if(!isset($_SESSION['user'])){
     header("Location:user_login.php");
